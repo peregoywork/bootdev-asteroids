16fb28aa403dd1cda14fe2ec67f2ec9481bcc33b
matthew bootdev <peregoywork@gmail.com>
2025-12-17 14:18:36 -0800

    following along... just follow along

---
+++ main.py
@@ -0,0 +1,36 @@
+import pygame
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from logger import log_state
+
+
+
+def print_startup():
+    print("Hello from astroids!")
+    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+
+def initialize_game():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    return screen
+
+def run_loop(screen):
+    while True:
+        log_state()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill('black')
+        pygame.display.flip()
+    
+
+
+def main():
+    print_startup()
+    screen = initialize_game()
+    run_loop(screen)
+    
+if __name__ == "__main__":
+    main()
